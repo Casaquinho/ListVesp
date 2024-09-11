980dec41f02fd782d6f1a6a26d4f1375f5c049e1
Casaquinho <marcelohmanzo@gmail.com>
2024-09-11 14:45:03 -0300

    Initial Commit

---
+++ ListVesp/ListVespApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListVespApp.swift
+//  ListVesp
+//
+//  Created by MARCELO HENRRIQUE MORELLO MANZO on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ListVespApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
